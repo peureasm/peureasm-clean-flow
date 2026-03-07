13fdcfd58ee8e09fa57e7a4a633b1da82209dbc5
Cori Jeon <coricogi76@gmail.com>
2026-03-07 05:37:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2289765446-e83f9",
+  "appId": "1:1013651426735:web:c39e5ec27757f60433b35a",
+  "apiKey": "AIzaSyAEr74RATjNbix3kbNT_Jnj6XWHJ_aFaFM",
+  "authDomain": "studio-2289765446-e83f9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1013651426735"
+};
