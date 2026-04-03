a4e89d770bc6d515b29b17761698d41756db732f
Cori Jeon <coricogi76@gmail.com>
2026-04-03 07:56:12 +0000

    모바일에서 사이드바 오픈 후, 배경 릭해도 닺히지 않아

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -560,7 +561,7 @@ const SidebarMenuButton = React.forwardRef<
     ref
   ) => {
     const Comp = asChild ? Slot : "button"
-    const { isMobile, state } = useSidebar()
+    const { isMobile, state, setOpenMobile } = useSidebar()
 
     const button = (
       <Comp
@@ -569,6 +570,10 @@ const SidebarMenuButton = React.forwardRef<
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+        onClick={(e) => {
+          if (isMobile) setOpenMobile(false)
+          props.onClick?.(e)
+        }}
         {...props}
       />
     )
@@ -720,6 +725,7 @@ const SidebarMenuSubButton = React.forwardRef<
   }
 >(({ asChild = false, size = "md", isActive, className, ...props }, ref) => {
   const Comp = asChild ? Slot : "a"
+  const { isMobile, setOpenMobile } = useSidebar()
 
   return (
     <Comp
@@ -735,6 +741,10 @@ const SidebarMenuSubButton = React.forwardRef<
         "group-data-[collapsible=icon]:hidden",
         className
       )}
+      onClick={(e) => {
+        if (isMobile) setOpenMobile(false)
+        props.onClick?.(e as any)
+      }}
       {...props}
     />
   )
